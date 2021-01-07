f3e466490a7ec2b19068fdadc92288363282cfd2
QYJY4737 <qyjy4737@163.com>
2021-01-07 16:12:14 +0800

    2021-1-7-16:12 添加BaseEntity

---
+++ springboot-jpa/src/main/java/cn/congee/api/domain/entity/BaseEntity.java
@@ -0,0 +1,19 @@
+package cn.congee.api.domain.entity;
+
+import java.io.Serializable;
+
+/**
+ * 基类
+ *
+ * @Date: 2021/1/7
+ * @Time: 15:54
+ * @author: wgb
+ **/
+public abstract class BaseEntity implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    public BaseEntity() {
+    }
+
+}
