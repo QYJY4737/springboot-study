856f342656f3dc81d89ad198486db64aac2577ce
QYJY4737 <qyjy4737@163.com>
2021-01-07 16:17:34 +0800

    2021-1-7-16:17 添加BaseDic

---
+++ springboot-jpa/src/main/java/cn/congee/api/common/enums/BaseDic.java
@@ -0,0 +1,26 @@
+package cn.congee.api.common.enums;
+
+/**
+ * 字典常量表
+ *
+ * @Date: 2021/1/7
+ * @Time: 16:16
+ * @author: wgb
+ **/
+public interface BaseDic {
+
+    /**
+     * 字典code
+     *
+     * @return
+     */
+    String getCode();
+
+    /**
+     * 字典名称
+     *
+     * @return
+     */
+    String getName();
+
+}
